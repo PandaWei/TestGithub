78195f75acfa0a4b2bb90ff6cf8ff268196fae73
weijg.fnst <weijg.fnst@cn.fujitsu.com>
2013-08-14 22:45:46 +0800

    modify testglob.c

--- testglob.c
+++ testglob.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <glob.h>
+#include <glob.h>   //头文件很重要！
 
-
+//查找制定路径下匹配的文件
 void test_glob(int argc, char** argv)
 {
     glob_t glo;
     int i, j;
-    for(i=0; i < argc; i++)
+    for(i=1; i < argc; i++)
     {
         glo.gl_offs=0;
         glob(argv[i], GLOB_TILDE, 0, &glo);
